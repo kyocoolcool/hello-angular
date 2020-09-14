9a1cdd14e23a4441fdc22e5f108e8b50024e7db5
kyocoolcool <kyocoolcool@hotmail.com>
2020-09-15 00:05:05 +0800

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,28 +7,30 @@ import {Component, Inject, OnInit} from '@angular/core';
     <div>
       <form #formRef="ngForm" (ngSubmit)="onSubmit(formRef.value)">
         <fieldset ngModelGroup="login">
-          <input name="userName" required #userNameRef="ngModel" type="text" [(ngModel)]="userName" minlength="3"/>
+          <input name="userName" required #userNameRef="ngModel" type="text" [(ngModel)]="userName" minlength="3"
+                 (focus)="onFocusUserName()"/>
           <div *ngIf="userNameRef.errors?.required">this is required</div>
           <div *ngIf="userNameRef.errors?.minlength">should be at least 3 characters</div>
           <br>
           <br>
-          <input name="userPassword" required #userPasswordRef="ngModel" type="password" [(ngModel)]="userPassword" minlength="3">
+          <input name="userPassword" required #userPasswordRef="ngModel" type="password" [(ngModel)]="userPassword" minlength="3"
+                 (focus)="onFocusPassword()">
           <div *ngIf="userPasswordRef.errors?.required">this is required</div>
           <div *ngIf="userPasswordRef.errors?.minlength">should be at least 3 characters</div>
           <br>
           <br>
-<!--          <button (click)="onClick()">login</button>-->
-<!--          <button type="submit" (click)="onSubmit(formRef)">submit</button>-->
           <button type="submit">submit</button>
         </fieldset>
       </form>
     </div>
   `,
-  styles: [],
+  styles: ['input.ng-invalid { border: 3px solid red; }',
+    'input.ng-valid { border: 3px solid green; }'
+  ],
 })
 export class LoginComponent implements OnInit {
-  userName = 'chris';
-  userPassword = '12345';
+  userName = '請輸入用戶名';
+  userPassword = '請輸入密碼';
 
   constructor(@Inject('auth') private services) {
   }
@@ -47,6 +49,17 @@ export class LoginComponent implements OnInit {
   onSubmit(value: any): void {
     // tslint:disable-next-line:no-console
     console.info(value.login.userName);
+    // tslint:disable-next-line:no-console
     console.info(value.login.userPassword);
+  }
+
+  // tslint:disable-next-line:typedef
+  onFocusUserName() {
+    this.userName = '';
+  }
+
+  // tslint:disable-next-line:typedef
+  onFocusPassword() {
+    this.userPassword = '';
   }
 }
